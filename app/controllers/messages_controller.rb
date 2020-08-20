624e51949fe3474614e077e75cec8dd075524d86
airi0623 <marcy.0623.doi@gmail.com>
2020-08-20 21:41:53 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,9 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
+
 
   def create
     @room = Room.find(params[:room_id])
@@ -10,6 +12,7 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path(@room)
     else
+      @messages = @room.messages.includes(:user)
       render :index
     end
   end
@@ -17,7 +20,7 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:content).merge(user_id: current_user.id )
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
     # ログインしているユーザーのid（user_id: current_user.id)と紐づいているデータを受け取れるよう許可する。
   end
 end
