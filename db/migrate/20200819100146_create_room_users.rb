ae3e87e2fff79e93d9d6a8dd290601211ac643fe
airi0623 <marcy.0623.doi@gmail.com>
2020-08-20 09:00:11 +0900

    プルダウンの実装＊

--- db/migrate/20200819100146_create_room_users.rb
+++ db/migrate/20200819100146_create_room_users.rb
@@ -1,8 +1,8 @@
 class CreateRoomUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :room_users do |t|
-      t.references :room, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: true
+      t.references :room, foreign_key: true
+      t.references :user, foreign_key: true
       # room.id,user.idとしないんだ。テーブル同士の参照を意味してるからカラムで特定しないのかな。勝手にidだけ取ってきてくれるんだろうか。
       t.timestamps
       # カリキュラムに null: false入ってなかったけど、リードミーを作成した際に書いたので入れた。
