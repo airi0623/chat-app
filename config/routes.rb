c6fb40283db5c33010f77a18c596a9bf60624154
airi0623 <marcy.0623.doi@gmail.com>
2020-08-20 16:21:50 +0900

    チャット画面の編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
-  root "messages#index"
+  root "rooms#index"
   #http://localhost:3000/にアクセスした際に表示されるページを指定している
   resources :users, only: [:edit, :update]
   #ユーザー編集に必要なルーティングは、editとupdate
